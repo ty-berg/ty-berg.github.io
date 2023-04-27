2eeeb18b2121de9aa9512e99e095d312799e41d9
Tyler Berg <tb2867@columbia.edu>
2023-04-27 17:49:18 -0400

    change text for buttons

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function lightSwitch(){
         document.getElementById("heading").className = "color-changing-dark";
         document.getElementById("light-or-dark").className = "button-dark";
         document.getElementById("animations").className = "button-dark";
-
+        document.getElementById("light-or-dark").innerHTML = "DARK MODE";
     }
     else{
         document.getElementById("body").className = "light";
@@ -21,12 +21,14 @@ function lightSwitch(){
         document.getElementById("heading").className = "color-changing-light";
         document.getElementById("light-or-dark").className = "button-light";
         document.getElementById("animations").className = "button-light";
+        document.getElementById("light-or-dark").innerHTML = "LIGHT MODE";
     }
 }
 
 function animationSwitch(){
     if(document.getElementById("heading").className=="color-changing-light" || document.getElementById("heading").className=="color-changing-dark"){
         document.getElementById("heading").className = "";
+        document.getElementById("animations").innerHTML = "ANIMATIONS OFF";
     }
     else{
         if(document.getElementById("body").className=="light"){
@@ -35,6 +37,7 @@ function animationSwitch(){
         else{
             document.getElementById("heading").className = "color-changing-dark";
         }
+        document.getElementById("animations").innerHTML = "ANIMATIONS ON";
     }
 }
 
