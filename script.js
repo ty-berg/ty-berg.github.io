7cb5cc9d063a119e22e0fa5f64ab2fd060a983b1
Tyler Berg <tb2867@columbia.edu>
2023-04-26 23:15:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function lightSwitch(){
+    if(document.getElementById("body").className=="light"){
+        document.getElementById("body").className = "dark";
+        document.getElementById("nav").className = "nav-links-dark";
+        document.getElementById("nav1").className = "nav-links-dark";
+        document.getElementById("nav2").className = "nav-links-dark";
+        document.getElementById("nav3").className = "nav-links-dark";
+        document.getElementById("nav4").className = "nav-links-dark";
+        document.getElementById("heading").className = "color-changing-dark";
+        document.getElementById("light-or-dark").className = "button-dark";
+        document.getElementById("animations").className = "button-dark";
+
+    }
+    else{
+        document.getElementById("body").className = "light";
+        document.getElementById("nav").className = "nav-links-light";
+        document.getElementById("nav1").className = "nav-links-light";
+        document.getElementById("nav2").className = "nav-links-light";
+        document.getElementById("nav3").className = "nav-links-light";
+        document.getElementById("nav4").className = "nav-links-light";
+        document.getElementById("heading").className = "color-changing-light";
+        document.getElementById("light-or-dark").className = "button-light";
+        document.getElementById("animations").className = "button-light";
+    }
+}
+
+function animationSwitch(){
+    if(document.getElementById("heading").className=="color-changing-light" || document.getElementById("heading").className=="color-changing-dark"){
+        document.getElementById("heading").className = "";
+    }
+    else{
+        if(document.getElementById("body").className=="light"){
+            document.getElementById("heading").className = "color-changing-light";
+        }
+        else{
+            document.getElementById("heading").className = "color-changing-dark";
+        }
+    }
+}
+
